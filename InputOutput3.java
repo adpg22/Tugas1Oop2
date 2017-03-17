3a86cf88837da41c27c1ac912f2d4d07fb4ac3a8
adpg22 <adwi3207@gmail.com>
2017-03-17 21:29:07 +0700

    Add files via upload

---
+++ InputOutput3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class InputOutput3 {
+	public static void main (String[] args) {
+		Scanner input = new Scanner (System.in);
+		String nama;
+		
+	System.out.print("Masukkan Nama : ");
+	nama = input.nextLine();
+	System.out.print(nama);
+	}
+}
